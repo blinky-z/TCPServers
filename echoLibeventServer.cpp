0a645f5e63e4c89167a3f6f6e7b76272fefefb7c
TwITe <dm.blink19@gmail.com>
2018-07-18 22:21:38 +0300

    update echo libevent server

--- echoLibeventServer.cpp
+++ echoLibeventServer.cpp
@@ -1,6 +1,7 @@
 #include <cstring>
 #include <cstdlib>
 #include <cstdio>
+#include <string>
 #include <cerrno>
 
 #include <event2/listener.h>
@@ -9,6 +10,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <iostream>
 
 void echo_read_cb(struct bufferevent* bev, void* ctx) {
     struct evbuffer* input = bufferevent_get_input(bev);
@@ -53,19 +55,56 @@ void accept_error_cb(struct evconnlistener* listener, void* ctx) {
 }
 
 int main(int argc, char* argv[]) {
+    const char** methods = event_get_supported_methods();
+    printf("Starting Libevent %s.  Available methods are:\n", event_get_version());
+    for (int i = 0; methods[i] != NULL; ++i) {
+        printf("    %s\n", methods[i]);
+    }
+    std::cout << std::endl;
+
+    struct event_base* base;
+
+    unsigned short int port = std::stoi(argv[1]);
+    std::string backendMethod = argv[2];
     struct sockaddr_in sockAddr;
     sockAddr.sin_family = AF_INET;
-    sockAddr.sin_port = htons(8080);
+    sockAddr.sin_port = htons(port);
     sockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    struct event_base* base = event_base_new();
+    struct event_config* cfg = event_config_new();
+
+    if (backendMethod == "select") {
+        event_config_avoid_method(cfg, "poll");
+        event_config_avoid_method(cfg, "epoll");
+    }
+    if (backendMethod == "poll") {
+        event_config_avoid_method(cfg, "select");
+        event_config_avoid_method(cfg, "epoll");
+    }
+    if (backendMethod == "epoll") {
+        event_config_avoid_method(cfg, "select");
+        event_config_avoid_method(cfg, "poll");
+    }
+
+    base = event_base_new_with_config(cfg);
+    event_config_free(cfg);
+
+    if (!base) {
+        std::cerr << "EvListener with using of " << backendMethod << " backend method creation failure" << std::endl;
+        return -1;
+    } else {
+        std::cout << "EvListener with using of " << backendMethod << " backend method has been created" << std::endl;
+    }
 
     struct evconnlistener* listener = evconnlistener_new_bind(base, accept_conn_cb, NULL,
                                                               LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE, -1,
                                                               (struct sockaddr*) &sockAddr, sizeof(sockAddr));
+
+    std::cout << "EvListener listens on PORT " << port << std::endl;
+
     evconnlistener_set_error_cb(listener, accept_error_cb);
 
-    event_base_loop(base, 0);
+    event_base_dispatch(base);
 
     return 0;
 }
